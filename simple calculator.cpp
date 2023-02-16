2bbf923b8534421fe9a951ff58cc87c9a3738e1a
Aditya Rawat <119433044+not-AdityaRawat@users.noreply.github.com>
2023-02-17 00:53:28 +0530

    Add files via upload

---
+++ simple calculator.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum,difference,multiply;
+  float divide;
+  
+  printf("Insert any number-\n",a);
+  scanf("%d",&a);
+  
+  printf("Insert any number-\n",b);
+  scanf("%d",&b);
+  
+  sum=a+b;
+  printf("your sum is %d\n",sum);
+  
+  difference=a-b;
+  printf("your difference is %d\n",difference);
+  
+  multiply=a*b;
+  printf("your product is %d\n",multiply);
+  
+  divide=a/b;
+  printf("your devide is %f\n",divide);
+  
+  
+   return 0;
+} 
